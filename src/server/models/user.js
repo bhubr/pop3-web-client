583af9aada4ace26dbdc1238f9f11320b51c6718
Benoît Hubert <benoithubert@gmail.com>
2018-02-08 15:09:03 +0100

    Create user works

--- src/server/models/user.js
+++ src/server/models/user.js
@@ -1,26 +1,69 @@
-const pool = require('../db');
+import pool from '../db';
+import bcrypt from 'bcrypt';
+import Promise from 'bluebird';
 
-const User = {
+const hashAsync = Promise.promisify(bcrypt.hash);
+const SALT_ROUNDS = 10;
 
-  readAll: function() {
-    return  pool
-      .query('select `id`, `email` from users');
-  },
+function trimAndQuote(v) {
+  return typeof v === 'string' ?
+    "'" + v.trim() + "'" : v;
+}
 
-  create: function(email) {
-    const insertQuery = "insert into users(email) values('" + email.trim() + "')";
-    let selectQuery = "select `id`, `email` from users where id = ";
+function passLog(v) {
+  console.log(v); return v;
+}
+
+export default class User {
+
+  static hashPassword(password) {
+    return hashAsync(password, SALT_ROUNDS);
+  }
+
+  static readAll() {
     return pool
-      .query(insertQuery)
-      .then(result => pool.query(selectQuery + result.insertId))
-      .then(records => (records[0]));
-  },
+      .query('select `id`, `email` from users');
+  }
 
-  delete: function(id) {
+  static beforeCreate(user) {
+    return User.hashPassword(user.password)
+      .then(password => Object.assign(user, {
+        password
+      }));
+  }
+
+  static create(user) {
+    const requiredKeys = ['email', 'password'];
+    const optionalKeys = ['first_name', 'last_name'];
+    for(let i = 0 ; i < requiredKeys.length ; i++) {
+      const k = requiredKeys[i];
+      if(! user[k]) {
+        return Promise.reject(new Error(`required key '${k}' is missing`));
+      }
+    }
+    for(let k in user) {
+      if(requiredKeys.indexOf(k) === -1 && optionalKeys.indexOf(k) === -1) {
+        return Promise.reject(new Error(`unexpected key '${k}'`));
+      }
+    }
+
+    return User.beforeCreate(user)
+      .then(user => {
+        const fields = Object.keys(user).join(',');
+        const values = Object.values(user).map(trimAndQuote).join(',');
+        const insertQuery = `insert into users(${fields}) values(${values})`;
+        let selectQuery = "select `id`, `email` from users where id = ";
+        return pool
+          .query(insertQuery)
+          .then(result => pool.query(selectQuery + result.insertId))
+          .then(records => (records[0]));
+      })
+  }
+
+  static delete(id) {
     const deleteQuery = 'delete from users where id = ' + id;
     return pool
       .query(deleteQuery);
   }
-};
 
-module.exports = User;
+}
--- tests/models/user.test.js
+++ tests/models/user.test.js
@@ -1,14 +1,59 @@
 const chai = require('chai');
 const expect = chai.expect;
-const User = require('../../dist/models/user');
+const pool = require('../../dist/db');
+const User = require('../../dist/models/user').default;
+
+
+const getId = (() => {
+  let id = 0;
+  return () => (++id);
+})();
+
+const getEmail = (() => {
+  return () => 'test.user.' + getId() + '@example.com';
+})();
+
 
 describe('User model test', () => {
 
-  it('reads all users', () => {
+  before(() => pool.query('truncate table users'));
+
+  it('hashes password', () =>
+    User.hashPassword('unsecure')
+    .then(hash => {
+      expect(hash).to.be.a('string');
+      expect(hash.length).to.be.equal(60);
+    })
+  );
+
+  it('creates a user with empty pass', () =>
+    User.create({
+      email: getEmail(), password: ''
+    })
+    .then(() => {
+      expect(false).to.be.equal(true);
+    })
+    .catch(err => {
+      expect(err.message).to.be.equal("required key 'password' is missing")
+    })
+  );
+
+  it('creates a user', () =>
+    User.create({
+      email: getEmail(), password: 'unsecure'
+    })
+    .then(user => {
+      expect(user).to.be.a('object');
+      expect(user.email).to.equal('test.user.2@example.com');
+      expect(user.password).to.be.a('undefined');
+    })
+  );
+
+  it('reads all users', () =>
     User.readAll()
     .then(users => {
-      expect(users.length).to.equal(0);
+      expect(users.length).to.equal(1);
     })
-  });
+  );
 
 });
