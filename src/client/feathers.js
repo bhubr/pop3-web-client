871155919c84685ae2a2d4abcd71b0b3735afbc0
Benoît Hubert <benoithubert@gmail.com>
2018-02-08 11:51:28 +0100

    remove feathersjs dependencies

--- src/client/feathers.js
+++ src/client/feathers.js
@@ -1,10 +1,10 @@
 // Inspired by FeathersJS docs and:
 // https://github.com/Alexisvt/featherjs-react-seed/blob/master/app/common/index.js
-import io from 'socket.io-client';
-import feathers from '@feathersjs/feathers';
-import socketio from '@feathersjs/socketio-client';
-import localstorage from 'feathers-localstorage';
-import authentication from '@feathersjs/authentication-client';
+// import io from 'socket.io-client';
+// import feathers from '@feathersjs/feathers';
+// import socketio from '@feathersjs/socketio-client';
+// import localstorage from 'feathers-localstorage';
+// import authentication from '@feathersjs/authentication-client';
 
 // const socket = io('http://localhost:3008/');
 // const app = feathers()
